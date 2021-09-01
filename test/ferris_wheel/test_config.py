bf02c8e6762fe25d923a4f6488c2fad355c826d4
Travis Chase <travis.chase@gmail.com>
2021-09-01 01:01:59 +0000

    tests for ferris wheel config

--- test/ferris_wheel/test_config.py
+++ test/ferris_wheel/test_config.py
@@ -8,9 +8,21 @@ class TestFerrisWheelConfig(unittest.TestCase):
     def setup(self):
         pass
     
-    def test_object(self):
+    def test_speed_limit(self):
         ferris_wheel_config = FerrisWheelConfig()
         self.assertTrue(ferris_wheel_config.speed_limit > 0 and ferris_wheel_config.speed_limit <= 100, "speed_limit config is within acceptable range")
+    
+    def test_speed_increment(self):
+        ferris_wheel_config = FerrisWheelConfig()
+        self.assertTrue(ferris_wheel_config.speed_increment > 0 and ferris_wheel_config.speed_increment <= 100, "speed_increment config is within acceptable range")
+    
+    def test_max_degrees(self):
+        ferris_wheel_config = FerrisWheelConfig()
+        self.assertTrue(ferris_wheel_config.max_degrees > 0 and ferris_wheel_config.max_degrees <= 360, "max_degrees config is within acceptable range")
+    
+    def test_check_button_press_wait_seconds(self):
+        ferris_wheel_config = FerrisWheelConfig()
+        self.assertTrue(ferris_wheel_config.check_button_press_wait_seconds > 0, "check_button_press_wait_seconds config is within acceptable range")
 
 if __name__ == '__main__':
     unittest.main()
